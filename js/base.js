49c856677d9d9fda9698a037bb59c52bd58f715e
unknown <>
2015-11-07 18:28:26 +0800

    a little file

---
+++ js/base.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+//  ready
+
+function ready(fn)
+{
+	if(document.addEventListener)
+	{
+		document.addEventListener('DOMContentLoaded',fn,false);
+	}
+	else
+	{
+		document.attachEvent('onreadystatechange',function (){
+			if(document.readyState == 'complete')
+			{
+				fn();
+			}	
+		});	
+	}
+}
--- js/js.js
+++ js/js.js
@@ -1,36 +1,51 @@
 // JavaScript Document
 
-//  ready
-function ready(fn)
-{
-	if(document.addEventListener)
-	{
-		document.addEventListener('DOMContentLoaded',fn,false);
-	}
-	else
-	{
-		document.attachEvent('onreadystatechange',function (){
-			if(document.readyState == 'complete')
-			{
-				fn();
-			}	
-		});	
-	}
-}
 
 
 
 //   start
 ready(function (){
 	var oMyself=document.getElementById('myself');
+	var oTwo=document.getElementById('two')
+	var oThree=document.getElementById('three')
+	var oWork=document.getElementById('work')
 	
 	//  屏幕滚动
 	window.onscroll=function (){
+		
+		//滚动距离
 		var scrollTop=document.documentElement.scrollTop || document.body.scrollTop;
-				
-		oMyself.style.background='rgba(0,0,0,'+(1-scrollTop/650).toFixed(1)+')';
+		
+		
+		// 导航条变化
+		if(scrollTop>350 && scrollTop<1900)
+		{
+			oTwo.style.top=0;
+		}
+		else if(scrollTop<350 || scrollTop>1900)
+		{
+			oTwo.style.top='-650px';
+		}
+		
+		
+		document.onmousewheel=function (ev)
+		{
+			oEvent=ev || event;	
+			
+			if(oEvent.wheelDelta<0)
+			{
+				oWork.style.top=(300-scrollTop)+'px';	
+			}
+			else if(oEvent.wheelDelta>0)
+			{
+				oWork.style.top=(300-scrollTop)+'px';
+			}
+			
+			
+		};
 		
 	};
+	
 	
 		
 })
